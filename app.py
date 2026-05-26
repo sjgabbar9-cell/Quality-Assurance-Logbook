0b59661bc68dcdc0027e10c615a156b3c51c64d5
sjgabbar9-cell <sjgabbar9@gmail.com>
2026-05-26 13:46:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ if "selected_row" not in st.session_state:
 if st.session_state.user is None:
 
     st.title("QA Logbook Login")
-    if os.path.exists("logo.png"):
-    st.image("logo.png", width=150)
+     if os.path.exists("logo.png"):
+     st.image("logo.png", width=150)
 
     uid=st.text_input("User ID")
     pwd=st.text_input("Password", type="password")
