cd4a6bcb58ce51f618b7b1655e610de35b42979d
sjgabbar9-cell <sjgabbar9@gmail.com>
2026-05-26 11:32:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,6 +145,13 @@ elif st.session_state.page=="entry":
             "Diagonal Variation:",
             table["Diag Max"].max() - table["Diag Min"].min()
         )
+        
+["SS Min"] = st.number_input("S/S Min", key="ss_min")
+d["SS Max"] = st.number_input("S/S Max", key="ss_max")
+
+d["CC Min"] = st.number_input("C/C Min", key="cc_min")
+d["CC Max"] = st.number_input("C/C Max", key="cc_max")
+
 
     # -------- PLANARITY ✅ BACK HERE --------
     st.subheader("Planarity Measurement")
